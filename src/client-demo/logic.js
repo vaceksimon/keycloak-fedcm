fb5dc559871bab0a3511914801274ce2597be4c1
Šimon Vacek <simonvacky@email.cz>
2024-04-28 21:45:47 +0200

    Client demo upgrade

--- src/client-demo/logic.js
+++ src/client-demo/logic.js
@@ -1,11 +1,11 @@
-let clientId = "example-client"
+let clientId = "example-client";
+
 async function login() {
     let nonce = 123456;
 	let credential;
 	try {
 	    credential = await navigator.credentials.get({
 	        identity: {
-				context: "signup",
                 providers: [{
                     configURL: "http://localhost:8080/realms/fedcm-realm/fedcm/config.json",
                     clientId: clientId,
@@ -26,11 +26,8 @@ async function login() {
 	    document.getElementById('message').innerHTML="";
 	    document.getElementById('error').innerHTML="Error during login :(";
 	}
-	decodedToken = getJSONIdToken(credential);
-	
-	document.getElementById('status').innerHTML="Token: " + JSON.stringify(decodedToken);
-	document.getElementById('message').innerHTML="Hello " + decodedToken.given_name + " " + decodedToken.family_name + ". I have stolen your identity :)";
-	    document.getElementById('error').innerHTML="";
+	localStorage.setItem('accessToken', credential.token);
+    document.dispatchEvent(new CustomEvent("loginStatus", {detail: "login"}))
 }
 
 async function logout() {
@@ -40,16 +37,14 @@ async function logout() {
 		    clientId: clientId,
 		    accountHint: "test@test.com",
 		});
-
-	    document.getElementById('status').innerHTML="Logout was successful";
-	    document.getElementById('message').innerHTML="";
-		document.getElementById('error').innerHTML="";
 	}
 	catch(e) {
 		document.getElementById('error').innerHTML="Error during logout :(";
 		document.getElementById('status').innerHTML="";
 		document.getElementById('message').innerHTML="";
 	}
+	localStorage.removeItem("accessToken");
+	document.dispatchEvent(new CustomEvent("loginStatus", {detail: "logout"}))
 }
 
 function base64UrlDecode(input) {
@@ -73,6 +68,42 @@ function base64UrlDecode(input) {
     }
 }
 
-function getJSONIdToken(credential) {
-	return JSON.parse(base64UrlDecode(credential.token.split('.')[1]));
+function getJSONToken() {
+    const token = localStorage.getItem('accessToken');
+    if (token === null) {
+        return false
+    }
+	return JSON.parse(base64UrlDecode(token.split('.')[1]));
 }
+
+function renderNavbar() {
+    accessToken = getJSONToken();
+    const loginButton = document.getElementById('loginButton');
+    const profileButton = document.getElementById('profileButton');
+    if (accessToken === false) {
+        loginButton.innerHTML = "Login";
+        loginButton.setAttribute("onclick", "login()");
+        loginButton.setAttribute("class", "btn btn-outline-primary")
+        profileButton.setAttribute("class", "btn btn-secondary")
+    }
+    else {
+        loginButton.innerHTML = "Logout";
+        loginButton.setAttribute("onclick", "logout()");
+        loginButton.setAttribute("class", "btn btn-outline-danger")
+        profileButton.setAttribute("class", "btn btn-primary")
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    renderNavbar();
+});
+
+document.addEventListener("loginStatus", function() {
+    renderNavbar();
+});
+
+window.addEventListener("storage", function(e) {
+    if(e.key === "accessToken") {
+        renderNavbar();
+    }
+})
